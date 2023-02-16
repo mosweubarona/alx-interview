d90ae74c37ffbdd45afbb0c1594e54a3d9bb720d
mosweubarona <mosweubarona@gmail.com>
2023-02-16 18:58:13 +0000

    m fix link

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
 
+"""Transpose the matrix (rows become columns, columns become rows)"""
 def rotate_2d_matrix(matrix):
-    n = len(matrix)
-    # Transpose the matrix (rows become columns, columns become rows)
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    # Reverse each row to rotate clockwise
-    for i in range(n):
-        matrix[i] = matrix[i][::-1]
+    """
+       Rotates 2D matrix 90 degrees clockwise
+       Reverse each row to rotate clockwise
+    """
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topLeft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
